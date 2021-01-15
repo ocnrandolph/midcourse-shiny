fe4485cc055fc79411862115c7b662082f813582
Oluchi Randolph <ocnrando@gmail.com>
2021-01-14 20:12:57 -0600

    starting on shiny app skeleton

---
+++ mcp_shiny/refugee-resettlement-us/global.R
@@ -0,0 +1,18 @@
+# This file is run only once at the start of the app
+
+# Import all needed libraries
+library(dplyr)
+library(tidyverse)
+library(tidycensus)
+library(stringr)
+library(readxl)
+library(janitor)
+library(ggplot2)
+library(plotly)
+library(shiny)
+library(shinydashboard)
+
+
+
+
+
